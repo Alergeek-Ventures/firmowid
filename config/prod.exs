e0c940d0a02d1298847588c764d74dc5cb59938f
Franciszek Madej <franek@alergeek.ventures>
2025-01-01 14:26:10 +0100

    fix: allow websocket connection from fly.dev

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,8 @@ import Config
 config :firmowid, FirmowidWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   http: [port: {:system, "PORT"}],
-  url: [host: "new.firmowid.pl", port: 80]
+  url: [host: "new.firmowid.pl", port: 80],
+  check_origin: ["https://new.firmowid.pl", "https://firmowid-redux.fly.dev"]
 
 config :firmowid,
   uploads_bucket: "firmowid-uploads-prod"
