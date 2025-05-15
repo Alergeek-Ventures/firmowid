64fc1b2ed0807de24430c1db812b41a2889f2067
MiGoYAm <67077321+MiGoYAm@users.noreply.github.com>
2025-05-15 10:23:18 +0200

    fix(timetracker): user hiding when editing project name

--- lib/firmowid_web/live/timetracker_live/projects.ex
+++ lib/firmowid_web/live/timetracker_live/projects.ex
@@ -247,23 +247,29 @@ defmodule FirmowidWeb.TimetrackerLive.Projects do
 
   defp render_project_user_hours(assigns) do
     ~H"""
-    <%= for {id, user} <- @project_user_hours do %>
-      <div
-        id={id}
-        class={
-          classes([
-            "flex items-center bg-white rounded-md p-4 justify-between col-span-full",
-            user.removed_from_project && "bg-white/50 border border-greyButtonBg/50"
-          ])
-        }
-      >
-        <.render_profile user={user} />
-        <span>{trunc(user.time_worked / 60 / 60)} h</span>
-      </div>
-    <% end %>
+    <div
+      class="space-y-2 grid grid-cols-subgrid col-span-full auto-rows-min"
+      phx-update="stream"
+      id="project_user_hours"
+    >
+      <%= for {id, user} <- @project_user_hours do %>
+        <div
+          id={id}
+          class={
+            classes([
+              "flex items-center bg-white rounded-md p-4 justify-between col-span-full",
+              user.removed_from_project && "bg-white/50 border border-greyButtonBg/50"
+            ])
+          }
+        >
+          <.render_profile user={user} />
+          <span>{trunc(user.time_worked / 60 / 60)} h</span>
+        </div>
+      <% end %>
 
-    <div class="hidden only:block py-8 bg-white rounded-md text-center text-darkGrey text-sm col-span-full">
-      Brak danych o czasie pracy dla tego projektu.
+      <div class="hidden only:block py-8 bg-white rounded-md text-center text-darkGrey text-sm col-span-full">
+        Brak danych o czasie pracy dla tego projektu.
+      </div>
     </div>
     """
   end
@@ -273,34 +279,40 @@ defmodule FirmowidWeb.TimetrackerLive.Projects do
 
   defp render_project_edit(assigns) do
     ~H"""
-    <%= for user <- @project_users do %>
-      <div class="flex items-center bg-white rounded-md p-4 justify-between col-span-full">
-        <.render_profile user={user} />
-        <button
-          phx-click="delete_user"
-          phx-value-user_id={user.id}
-          class="h-6 w-8 rounded-md hover:bg-greyButtonBg text-darkGrey disabled:text-orangeText inline-flex items-center justify-center ml-1"
-        >
-          <.icon name="hero-trash-micro" />
-        </button>
-      </div>
-    <% end %>
+    <div
+      class="space-y-2 grid grid-cols-subgrid col-span-full auto-rows-min"
+      phx-update="stream"
+      id="project_user"
+    >
+      <%= for user <- @project_users do %>
+        <div class="flex items-center bg-white rounded-md p-4 justify-between col-span-full">
+          <.render_profile user={user} />
+          <button
+            phx-click="delete_user"
+            phx-value-user_id={user.id}
+            class="h-6 w-8 rounded-md hover:bg-greyButtonBg text-darkGrey disabled:text-orangeText inline-flex items-center justify-center ml-1"
+          >
+            <.icon name="hero-trash-micro" />
+          </button>
+        </div>
+      <% end %>
 
-    <form phx-change="add_user" class="relative col-span-full">
-      <.icon
-        name="hero-plus-mini"
-        class="text-darkGrey absolute top-1/2 transform -translate-y-1/2 left-2"
-      />
-      <.input
-        type="select"
-        name="user_id"
-        id="user"
-        prompt="Dodaj współpracownika"
-        value={nil}
-        options={Enum.map(@users, &{&1.name || &1.email, &1.id})}
-        class="pl-9"
-      />
-    </form>
+      <form phx-change="add_user" class="relative col-span-full">
+        <.icon
+          name="hero-plus-mini"
+          class="text-darkGrey absolute top-1/2 transform -translate-y-1/2 left-2"
+        />
+        <.input
+          type="select"
+          name="user_id"
+          id="user"
+          prompt="Dodaj współpracownika"
+          value={nil}
+          options={Enum.map(@users, &{&1.name || &1.email, &1.id})}
+          class="pl-9"
+        />
+      </form>
+    </div>
     """
   end
 
@@ -309,43 +321,49 @@ defmodule FirmowidWeb.TimetrackerLive.Projects do
   defp render_hours_records(assigns) do
     ~H"""
     <div
-      :for={{id, %{user: user, hours_record: record}} <- @hours_records}
-      class="grid grid-cols-subgrid col-span-full"
+      class="space-y-2 grid grid-cols-subgrid col-span-full auto-rows-min"
+      phx-update="stream"
+      id="hours_records"
     >
-      <div id={id} class="flex items-center bg-white rounded-md p-4 justify-between">
-        <.render_profile user={user} />
-        <span :if={record}>{trunc(record.number_of_hours)} h</span>
-      </div>
-      <div class="bg-white rounded-md p-4 justify-between items-center flex gap-5">
-        <%= if record do %>
-          <span class="text-xs font-semibold text-greenText bg-greenBg pl-2 pr-1 py-[5px] uppercase rounded-md flex items-center justify-between flex-1 gap-1">
-            EWIDENCJA <.icon name="hero-check-micro" />
-          </span>
-          <a
-            href={~p"/czasosledz/ewidencja/#{record.id}"}
-            download={"Ewidencja_#{record.year}_#{record.month}_#{user.name || user.email}.pdf"}
-            class="p-1 transition hover:bg-greyButtonBg rounded-md inline-flex items-center justify-center"
-          >
-            <.icon name="hero-arrow-down-tray-mini" class="text-darkGrey" />
-          </a>
-        <% else %>
-          <span class="text-xs font-semibold text-darkGrey bg-greyButtonBg px-2 py-[5px] uppercase rounded-md flex items-center justify-between flex-1 gap-1">
-            BRAK <.icon name="hero-x-mark-micro" />
-          </span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="stroke-greyButtonBg shrink-0 m-0.5"
-          >
-            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="m14.5 12.5-5 5" /><path d="m9.5 12.5 5 5" />
-          </svg>
-        <% end %>
+      <div
+        :for={{id, %{user: user, hours_record: record}} <- @hours_records}
+        class="grid grid-cols-subgrid col-span-full"
+      >
+        <div id={id} class="flex items-center bg-white rounded-md p-4 justify-between">
+          <.render_profile user={user} />
+          <span :if={record}>{trunc(record.number_of_hours)} h</span>
+        </div>
+        <div class="bg-white rounded-md p-4 justify-between items-center flex gap-5">
+          <%= if record do %>
+            <span class="text-xs font-semibold text-greenText bg-greenBg pl-2 pr-1 py-[5px] uppercase rounded-md flex items-center justify-between flex-1 gap-1">
+              EWIDENCJA <.icon name="hero-check-micro" />
+            </span>
+            <a
+              href={~p"/czasosledz/ewidencja/#{record.id}"}
+              download={"Ewidencja_#{record.year}_#{record.month}_#{user.name || user.email}.pdf"}
+              class="p-1 transition hover:bg-greyButtonBg rounded-md inline-flex items-center justify-center"
+            >
+              <.icon name="hero-arrow-down-tray-mini" class="text-darkGrey" />
+            </a>
+          <% else %>
+            <span class="text-xs font-semibold text-darkGrey bg-greyButtonBg px-2 py-[5px] uppercase rounded-md flex items-center justify-between flex-1 gap-1">
+              BRAK <.icon name="hero-x-mark-micro" />
+            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="stroke-greyButtonBg shrink-0 m-0.5"
+            >
+              <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="m14.5 12.5-5 5" /><path d="m9.5 12.5 5 5" />
+            </svg>
+          <% end %>
+        </div>
       </div>
     </div>
     """
