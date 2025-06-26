5db5be2f2152f7b6282bd6ca4005c0aa9cb0a8ea
Mirosław Jakieła <67077321+MiGoYAm@users.noreply.github.com>
2025-06-26 13:51:02 +0200

    fix(invoicing): navbar weird scaling with non default browser font size

--- lib/firmowid_web/components/core_components.ex
+++ lib/firmowid_web/components/core_components.ex
@@ -759,7 +759,7 @@ defmodule FirmowidWeb.CoreComponents do
     ~H"""
     <label class={classes(["flex w-44 justify-between gap-4 items-center h-full bg-greyButtonBg
         hover:border-darkGrey border border-transparent transition-colors
-        rounded-lg py-1 px-3 max-md:hidden has-[:disabled]:opacity-40", @class])}>
+        rounded-lg py-2 px-3 max-md:hidden has-[:disabled]:opacity-40", @class])}>
       <input
         type="button"
         phx-hook="AirDatepicker"
@@ -776,7 +776,7 @@ defmodule FirmowidWeb.CoreComponents do
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        class="max-md:hidden fill-darkGrey"
+        class="max-md:hidden fill-darkGrey size-[1.5em]"
       >
         <path d="M12 12H17V17H12V12ZM19 3H18V1H16V3H8V1H6V3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM19 5V7H5V5H19ZM5 19V9H19V19H5Z" />
       </svg>
