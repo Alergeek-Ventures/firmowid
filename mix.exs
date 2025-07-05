f311f937ec2e2cdd162d45707851a3c872f7d3f0
Franciszek Madej <franek@alergeek.ventures>
2025-07-05 14:51:27 +0200

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -87,7 +87,7 @@ defmodule Firmowid.MixProject do
       {:ecto_psql_extras, "~> 0.6"},
       {:any_ascii, "~> 0.3.2"},
       {:multipart, "~> 0.4.0"},
-      {:posthog, "~> 1.0.2"},
+      {:posthog, "~> 1.1.0"},
       {:reverse_proxy_plug, "~> 3.0"},
       {:ecto_dev_logger, "~> 0.14"}
     ]
