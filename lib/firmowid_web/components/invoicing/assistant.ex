031c5c5413556be3d54a535689b6d639e2585133
Adam Pliszka <adampliszka2@gmail.com>
2025-08-14 12:22:34 +0200

    fix(assistant): fixed query visibility

--- lib/firmowid_web/components/invoicing/assistant.ex
+++ lib/firmowid_web/components/invoicing/assistant.ex
@@ -130,7 +130,7 @@ defmodule FirmowidWeb.Components.Invoicing.Assistant do
         waluta: <span class="font-semibold">{@filters["currency"]}</span>
       </div>
       <div
-        :if={@filters["query"]}
+        :if={@filters["query"] in ["", nil]}
         class="py-1 px-3 bg-orange-100 text-orange-900 rounded whitespace-nowrap animate-fade-in"
       >
         fraza: <span class="font-semibold">{@filters["query"]}</span>
