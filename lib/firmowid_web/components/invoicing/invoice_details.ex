b1454755db98ae5170a130d2b216f2eddc27e9c8
Franciszek Madej <franek@alergeek.ventures>
2025-11-10 23:07:40 +0100

    chore: retrain model with more examples

--- lib/firmowid_web/components/invoicing/invoice_details.ex
+++ lib/firmowid_web/components/invoicing/invoice_details.ex
@@ -129,8 +129,8 @@ defmodule FirmowidWeb.Components.Invoicing.InvoiceDetails do
 
     predicition_level =
       cond do
-        assigns.prediction_score >= 0.9 and assigns.is_highest_green -> :high
-        assigns.prediction_score >= 0.53 -> :mid
+        assigns.prediction_score >= 0.92 and assigns.is_highest_green -> :high
+        assigns.prediction_score >= 0.87 -> :mid
         true -> :low
       end
 
