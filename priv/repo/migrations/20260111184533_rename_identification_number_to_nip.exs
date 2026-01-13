81021501296ad11e9d4c549fc821ecb420fb9322
Mirosław Jakieła <67077321+MiGoYAm@users.noreply.github.com>
2026-01-13 21:04:39 +0100

    chore: squash migrations related to ksef submission

--- priv/repo/migrations/20260111184533_rename_identification_number_to_nip.exs
+++
@@ -1,7 +0,0 @@
-defmodule Firmowid.Repo.Migrations.RenameIdentificationNumberToNip do
-  use Ecto.Migration
-
-  def change do
-    rename table(:organizations), :identification_number, to: :nip
-  end
-end
