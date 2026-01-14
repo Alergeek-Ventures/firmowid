c769980432925550f08714270c6ce394357815bc
Franciszek Madej <franek@alergeek.ventures>
2026-01-14 15:57:49 +0100

    fix(ksef): use production ksef on production of Firmowid

--- config/prod.exs
+++ config/prod.exs
@@ -26,7 +26,7 @@ config :firmowid, FirmowidWeb.Endpoint,
     exclude: ["localhost", "127.0.0.1"]
   ]
 
-config :firmowid, :ksef, base_url: "https://api-demo.ksef.mf.gov.pl/v2/"
+config :firmowid, :ksef, base_url: "https://ksef.mf.gov.pl/api/v2"
 
 # Do not print debug messages in production
 config :logger, level: :info
