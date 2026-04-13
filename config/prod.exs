d8b2b529c1f708a98054e501cd9ca690a17ebb65
Franciszek Madej <franek@alergeek.ventures>
2026-04-13 15:41:19 +0200

    chore: set in_app_otp_apps to help sentry with code recognition

--- config/prod.exs
+++ config/prod.exs
@@ -30,6 +30,7 @@ config :logger, level: :info
 config :sentry,
   dsn: "https://dc33e5660f563be6b0423120825f8da3@o4511195748630528.ingest.de.sentry.io/4511195751317584",
   environment_name: Mix.env(),
+  in_app_otp_apps: [:firmowid],
   enable_source_code_context: true,
   root_source_code_paths: [File.cwd!()],
   before_send_log: {Firmowid.SentryFilter, :before_send_log},
