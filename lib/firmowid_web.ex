271dd97427fe7e58b14a8af646e3156ea20f2552
Franciszek Madej <franek@alergeek.ventures>
2026-04-13 16:12:05 +0200

    fix(web): report LiveView crashes via Sentry in a better way

--- lib/firmowid_web.ex
+++ lib/firmowid_web.ex
@@ -57,6 +57,8 @@ defmodule FirmowidWeb do
         layout: {FirmowidWeb.Infrastructure.Layouts, :app},
         container: {:div, class: "min-h-full flex flex-col"}
 
+      on_mount Sentry.LiveViewHook
+
       unquote(html_helpers())
     end
   end
