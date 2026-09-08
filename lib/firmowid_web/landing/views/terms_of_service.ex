507f5264e11d4bd2260fe1461cbd0a51c7326e69
Konrad Guzek <konrad@guzek.uk>
2026-09-08 12:55:57 +0200

    docs(terms): bump change date

--- lib/firmowid_web/landing/views/terms_of_service.ex
+++ lib/firmowid_web/landing/views/terms_of_service.ex
@@ -30,7 +30,7 @@ defmodule FirmowidWeb.Landing.Views.TermsOfService do
     <.legal_page>
       <h1 class="text-4xl font-bold">Regulamin</h1>
       <p class="text-sm text-neutral-500">
-        Ostatnia aktualizacja: 7 września 2026 r.
+        Ostatnia aktualizacja: 8 września 2026 r.
       </p>
 
       <h2>§1. Postanowienia ogólne</h2>
