ba0a0476b5821a55a1af1704d481765ac9205060
Konrad Guzek <konrad@guzek.uk>
2026-09-11 10:18:13 +0200

    test(delegations): add trip details to settlement test

--- lib/firmowid_web/delegations/views/delegation_test.exs
+++ lib/firmowid_web/delegations/views/delegation_test.exs
@@ -19,6 +19,8 @@ defmodule FirmowidWeb.Delegations.Views.DelegationTest do
         %{
           title: "Wyjazd służbowy",
           billing_month: ~D[2026-08-01],
+          destination: "Kraków",
+          transport_types: [:railway],
           purpose: "Spotkanie z klientem",
           advance_payment_amount: Money.new(:PLN, 100),
           start_date: ~D[2026-08-10],
