b4947eb4d21257e29ea72697ce2f82477c752f76
Konrad Guzek <konrad@guzek.uk>
2026-09-14 18:29:16 +0200

    fix(delegations): generate all user initials in references

--- lib/firmowid/ash/delegations/delegation_test.exs
+++ lib/firmowid/ash/delegations/delegation_test.exs
@@ -7,12 +7,14 @@ defmodule Firmowid.Ash.Delegations.DelegationTest do
 
   alias Ash.Error.Forbidden
   alias Firmowid.Ash.Blobs.Blob
+  alias Firmowid.Ash.Core
   alias Firmowid.Ash.Delegations
   alias Firmowid.Ash.Delegations.DelegationExpense
   alias Firmowid.Ash.Scope
 
   setup do
     employee = user_fixture()
+    employee = Core.update_profile!(employee, %{name: "Kira Voss"}, actor: employee)
     admin = admin_fixture(%{organization_id: employee.organization_id})
 
     %{
@@ -35,8 +37,8 @@ defmodule Firmowid.Ash.Delegations.DelegationTest do
     first = Delegations.create_delegation!(delegation_attrs(), scope: employee_scope)
     second = Delegations.create_delegation!(delegation_attrs(), scope: employee_scope)
 
-    assert first.reference =~ ~r/^U[0-9A-F-]+-2026-09-1$/
-    assert second.reference =~ ~r/^U[0-9A-F-]+-2026-09-2$/
+    assert first.reference == "KV-2026-09-1"
+    assert second.reference == "KV-2026-09-2"
   end
 
   test "only an administrator can approve a delegation", %{
